9982c2fa1e8f3cb73f1f2b08ddde6fc1907054bd
BernardSaturn <beeh.geek@gmail.com>
2024-04-29 13:14:54 -0300

    Segundo commit do projeto

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -1,11 +1,19 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
+	
     public static void main(String[] args) {
+    	
+    	Conta c1 = new Conta(1, 123, 1, "Bernardo", 500000.0f);
+    	c1.visualizar();
+    	c1.sacar(15000.0f);
+    	c1.visualizar();
+    	c1.depositar(30000.0f);
+    	c1.visualizar();
         
         Scanner leia = new Scanner(System.in);
 		
